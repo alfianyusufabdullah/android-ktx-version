988bf7bf832e6fb0cf32cf3a6b6c23877786bd53
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2018-12-10 07:42:38 +0700

    fix app crash when no internet connection

--- app/src/main/java/alfianyusufabdullah/androidxversion/data/remote/RemoteDataSource.kt
+++ app/src/main/java/alfianyusufabdullah/androidxversion/data/remote/RemoteDataSource.kt
@@ -1,23 +1,28 @@
 package alfianyusufabdullah.androidxversion.data.remote
 
 import alfianyusufabdullah.androidxversion.data.entity.ModuleEntity
+import android.util.Log
 import org.jsoup.Jsoup
 
 class RemoteDataSource {
 
     fun getModules(module: String, url: String): List<ModuleEntity> {
-        val request = Jsoup.connect(url).execute()
-        val elements = request.parse().allElements
         val modules = mutableListOf<ModuleEntity>()
+        try {
+            val request = Jsoup.connect(url).execute()
+            val elements = request.parse().allElements
 
-        elements.forEach {
-            val tag = it.tagName()
-            if (tag.contains("-ktx")) {
-                val versions = it.attr("versions")
-                val lastVersion = versions.split(",").last()
+            elements.forEach {
+                val tag = it.tagName()
+                if (tag.contains("-ktx")) {
+                    val versions = it.attr("versions")
+                    val lastVersion = versions.split(",").last()
 
-                modules.add(ModuleEntity(module , tag , lastVersion))
+                    modules.add(ModuleEntity(module, tag, lastVersion))
+                }
             }
+        } catch (e: Exception){
+            Log.d("ERR" , e.localizedMessage)
         }
 
         return modules
