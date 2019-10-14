af513e885c4958570c6b001be2a6f2a0d1348e10
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-10-14 22:18:08 +0700

    add new property

--- app/src/main/java/alfianyusufabdullah/androidxversion/data/entity/ModuleEntity.kt
+++ app/src/main/java/alfianyusufabdullah/androidxversion/data/entity/ModuleEntity.kt
@@ -1,3 +1,8 @@
 package alfianyusufabdullah.androidxversion.data.entity
 
-data class ModuleEntity(var module: String, var packages: String, var version: String)
+data class ModuleEntity(
+    val module: String,
+    val packages: String,
+    val latestRelease: String,
+    val latestStableRelease: String
+)
